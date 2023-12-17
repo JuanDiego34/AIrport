05d0601b637a9950411b1583b9741348698d6002
Juan Diego Rendón Cachafeiro <72521316+JuanDiego34@users.noreply.github.com>
2023-12-17 17:40:27 +0000

    Update PassengerTest.java

--- src/test/java/es/ull/passengers/PassengerTest.java
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -6,36 +6,61 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @class PassengerTest
+ * @brief Contiene pruebas unitarias para verificar el comportamiento de la clase Passenger.
+ */
 class PassengerTest {
 
+    /**
+     * @brief Pasajero utilizado en las pruebas.
+     */
     private Passenger passenger;
 
+    /**
+     * @brief Configuración inicial antes de cada prueba.
+     */
     @BeforeEach
     void setUp() {
-        // Initialize a Passenger object before each test
+        // Inicializa un objeto Passenger antes de cada prueba
         passenger = new Passenger("ID123", "John Doe", "US");
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de getIdentifier().
+     */
     @Test
     void testGetIdentifier() {
         assertEquals("ID123", passenger.getIdentifier());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de getName().
+     */
     @Test
     void testGetName() {
         assertEquals("John Doe", passenger.getName());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de getCountryCode().
+     */
     @Test
     void testGetCountryCode() {
         assertEquals("US", passenger.getCountryCode());
     }
 
+    /**
+     * @brief Prueba para verificar que getFlight() devuelve null cuando el pasajero no está asociado a ningún vuelo.
+     */
     @Test
     void testGetFlight() {
         assertNull(passenger.getFlight());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de joinFlight().
+     */
     @Test
     void testJoinFlight() {
         Flight flight = new Flight("AB123", 50);
@@ -45,6 +70,9 @@ class PassengerTest {
         assertEquals(1, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de joinFlight() cuando el pasajero ya está en otro vuelo.
+     */
     @Test
     void testJoinFlightWithPreviousFlight() {
         Flight flight1 = new Flight("AB123", 50);
@@ -58,6 +86,9 @@ class PassengerTest {
         assertEquals(1, flight2.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de setFlight().
+     */
     @Test
     void testSetFlight() {
         Flight flight = new Flight("AB123", 50);
@@ -67,6 +98,9 @@ class PassengerTest {
         assertEquals(0, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Prueba para verificar el correcto funcionamiento de toString().
+     */
     @Test
     void testToString() {
         assertEquals("Passenger John Doe with identifier: ID123 from US", passenger.toString());
